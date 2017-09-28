299774e3b07be4b97359ccd49845b270e4b96cd2
usuario <usuario@8LIEST6>
2017-09-28 19:54:51 -0300

    Adicionando classe JogoDaVelhaCampeonato.

--- Lab03/src/JogoDaVelhaCampeonato.java
+++ Lab03/src/JogoDaVelhaCampeonato.java
@@ -6,24 +6,87 @@ import java.util.Scanner;
  * Aluno 3: CARLOS VINICIUS NASCIMENTO LIRA
  */
 
-public class JogoDaVelhaModularizado {
-	private static Scanner sc = new Scanner (System.in);
+public class JogoDaVelhaCampeonato {
+	private static Scanner sc = new Scanner(System.in);
+	
 
 	public static void main(String args[]){
-		
+		final int QNT_JOGADORES = 4;
 		final int SAIR = 0;
+		String jogadores[] = new String[QNT_JOGADORES];	
+		int vitorias[] = new int[QNT_JOGADORES];
+
 		
+		jogadores = incluiJogadores(jogadores, QNT_JOGADORES);
+
 		int op = 1;
 		do{
-			inicioJogo();
+		    imprimirJogadores(jogadores);
+			inicioJogo(jogadores, escolherJogador(1), escolherJogador(2), vitorias);
 			
 			System.out.print("\nPARA SAIR DIGITE '0' \nPARA CONTINUAR DIGITE QUALQUER INTEIRO: ");
 			op = sc.nextInt();
+			if(op == SAIR){
+			    imprimeRanking(jogadores, vitorias);
+			}
 		}while(op  != SAIR);
 		
 	}
 	
-	private static void inicioJogo(){
+	private static String[] incluiJogadores(String jogadores[], int qntJogadores){
+		for (int i = 0; i<qntJogadores; i++){
+			System.out.print("INSIRA O NOME DO " + (i+1) + "º JOGADOR: ");
+			jogadores[i] = sc.nextLine();
+		}
+		return jogadores;
+	}
+	
+	private static void imprimirJogadores(String jogadores[]){
+	    System.out.println("ESCOLHER JOGADORES:");
+	    for(int i = 0; i < jogadores.length; i++){
+	        System.out.println(i+1 + " - " + jogadores[i]);
+	    }
+	}
+	
+	private static int escolherJogador(int i){
+	    System.out.print("JOGADOR " + i + ": ");
+	    return (sc.nextInt() - 1);
+	}
+
+	private static void imprimeRanking(String jogadores[], int vit[]){
+
+		int ranking[] = new int[jogadores.length];
+		for(int i = 0; i<jogadores.length; i++){
+			ranking[i] = i;
+		}
+		
+		ranking = ordenarRankingInsertion(ranking, vit);
+		
+		
+		System.out.print("\n ------RANKING------ \nJOGADOR       Nº DE VITÓRIAS\n");
+		for(int i = 0; i<jogadores.length; i++){
+		    System.out.println(jogadores[ranking[i]] + " " + vit[ranking[i]]);
+		}
+	}
+	
+	private static int[] ordenarRankingInsertion(int vetor[], int vit[]){
+	    
+	    for(int j = 2; j < vit.length; j++){
+	        int chave = vit[j];
+	        int i = j-1;
+	        
+	        while(i>0 && vit[i]>chave){
+	            vit[i+1] = vit[i];
+	            vetor[i+1] = i;
+	            i--;
+	        }
+	        vit[i+1] = chave;
+	    }
+	    
+	    return vetor;
+	}
+	
+	private static void inicioJogo(String jogador[], int i, int j, int vitorias[]){
 		char tabuleiro[][] = new char[3][3];
 		final char O='O';
 		final char X='X';
@@ -36,10 +99,11 @@ public class JogoDaVelhaModularizado {
 
 	    while(jogada<=NUM_JOGADAS)
 	        {
-	    	recebeArgumentos(tabuleiro, jogada, O, X);
+	    	recebeArgumentos(tabuleiro, jogada, O, X, jogador, i, j);
 	    	jogada++;
 
-	        if (verificaVencedor(tabuleiro, jogada, O, X)){
+	        if (verificaVencedor(tabuleiro, jogada, O, X, jogador, i, j)){
+	        	somaVitorias(tabuleiro, O, X, vitorias, i, j);
 	        	break;
 	        }
 
@@ -85,11 +149,11 @@ public class JogoDaVelhaModularizado {
         
 	}
 	
-	private static void recebeArgumentos(char tabuleiro[][], int jogada, char O, char X){
+	private static void recebeArgumentos(char tabuleiro[][], int jogada, char O, char X, String jogadores[], int um, int dois){
 		int i, j;
 		System.out.println("\nINSIRA AS COORDENADAS");
-        if((jogada%2)==1)System.out.print("PLAYER 2\nLINHA: ");
-        else System.out.print("PLAYER 1\nLINHA: ");
+        if((jogada%2)==1)System.out.print(jogadores[dois] + "\nLINHA: ");
+        else System.out.print(jogadores[um] + "\nLINHA: ");
         
         sc = new Scanner (System.in);
 		i = sc.nextInt();
@@ -100,7 +164,7 @@ public class JogoDaVelhaModularizado {
         	imprimeTelaPrincipal(tabuleiro);
         }else{
         	imprimeTelaPrincipal(tabuleiro);
-        	recebeArgumentos(tabuleiro, jogada, O, X);
+        	recebeArgumentos(tabuleiro, jogada, O, X, jogadores, um, dois);
         }
         
 	}
@@ -145,26 +209,37 @@ public class JogoDaVelhaModularizado {
 		           (verificaDiagonais(tabuleiro, caractere)));
 	}
 	
-	private static boolean verificaVencedor(char tabuleiro[][], int jogada, char O, char X){
-		final int NUM_JOGADAS = 9;
+	private static boolean verificaVencedor(char tabuleiro[][], int jogada, char O, char X, String jogadores[], int i, int j){
 		if(verificaTabuleiro(tabuleiro, O))
         {
-        	System.out.print("\nJogador 1, VOCE VENCEU!!!");
+        	System.out.print("\n" + jogadores[i] + ", VOCE VENCEU!!!");
             return true;
         }
         if(verificaTabuleiro(tabuleiro, X))
         {
-        	System.out.print("\nJogador 2, VOCE VENCEU!!!");
+        	System.out.print("\n" + jogadores[j] + ", VOCE VENCEU!!!");
         	return true;
         }
 
-        if(jogada==NUM_JOGADAS)
+        if(jogada==9)
         {
         	System.out.print("\nPARTIDA EMPATADA");
         	return true;
         }
         
         return false;
+	}
+	
+	private static void somaVitorias(char tabuleiro[][], char O, char X, int vitorias[], int i, int j){
+		if(verificaTabuleiro(tabuleiro, O))
+        {
+            vitorias[i]++;
+        }
+        if(verificaTabuleiro(tabuleiro, X))
+        {
+        	vitorias[j]++;
+        }
+        
 	}
 	
 }
