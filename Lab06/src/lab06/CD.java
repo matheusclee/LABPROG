f103dc4a0312af9373fa361fd5372b4817b3d0a2
mathe <mathe@LAPTOP-8KDJ85DA>
2017-11-20 00:17:48 -0200

    Implemetação inicial do Lab06

---
+++ Lab06/src/lab06/CD.java
@@ -0,0 +1,172 @@
+/* 
+ * ALUNO 1: Matheus Clemente Pereira
+ * ALUNO 2: Carlos Vinicius
+ */
+
+package lab06;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Classe representada para usar um CD
+ * 
+ * @version 1.0
+ * @author Matheus Clemente/Carlos Vinicius
+ *
+ */
+public class CD {
+	
+	private String artista;
+	private String titulo;
+	private String trilhaPrincipal;
+	private int M;
+	private List<String> faixas = new ArrayList();
+	private final int VAZIO = 0;
+	private final int NUM_FAIXAS_PADRAO = 10;
+	
+	/**
+	 * Construtor de um CD
+	 * @param titulo - Titulo do CD
+	 * @param artista - Artista do CD
+	 * @param num_faixas - Quantidade de musicas no CD
+	 * @throws Exception - O numero de faixas nao pode ser menor que 1
+	 */
+	public CD(String titulo, String artista, int num_faixas) throws Exception {
+		this.titulo = titulo;
+		this.artista = artista;
+		setM(num_faixas);
+	}
+	
+	/**
+	 * Construtor de um CD
+	 * @param titulo - Titulo do CD
+	 * @param artista - Artista do CD
+	 */
+	public CD(String titulo, String artista){
+		this.titulo = titulo;
+		this.artista = artista;
+		M = NUM_FAIXAS_PADRAO;
+	}
+	
+	
+	private void setM(int num) throws Exception{
+		if(num < 1){
+			throw new Exception("Parametros passados estao incorretos");
+		}else{
+			M = num;
+		}
+	}
+	/**
+	 * Recupera o artista do CD
+	 * @return Retorna o artista
+	 */
+	public String getArtista(){
+		return artista;
+	}
+	
+	/**
+	 * Recupera o Titulo do CD
+	 * @return Retorna o titulo
+	 */
+	public String getTitulo(){
+		return titulo;
+	}
+	
+	/**
+	 * Recupera a Trilha Principal
+	 * @return Retorna a trilha principal
+	 */
+	public String getTrilhaPrincipal(){
+		return trilhaPrincipal;
+	}
+	
+	/**
+	 * Escolhe a Trilha Principal do CD
+	 * @param faixa - Passa a trilha principal
+	 * @throws Exception - A trilha informada deve existir no CD
+	 */
+	public void setTrilhaPrincipal(String faixa) throws Exception{
+		if(faixas.contains(faixa)){
+			trilhaPrincipal = faixa;
+		}else{
+			throw new Exception("Faixa informada nao existe.");
+		}
+	}
+	
+	/**
+	 * Recupera a quantida de faixas informada
+	 * @return - Retorna quantidade de faixas
+	 */
+	public int getM(){
+		return M;
+	}
+	
+	/**
+	 * Adiciona musicas ao CD
+	 * @param faixa - musica que sera adicionada
+	 * @return - Retorna true ou false
+	 */
+	public boolean setFaixas(String faixa){
+		if(this.faixas.size() < M){
+			this.faixas.add(faixa);
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	/**
+	 * Recupera a musica que esta na posicao informada, se nao existir retorna null
+	 * @param i - posicao desejada
+	 * @return - retorna o elemento da posicao i ou retorna null
+	 */
+	public String getI(int i) {
+		if(i < faixas.size()-1){
+			return faixas.get(i);
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Recupera informacoes do CD
+	 */
+	@Override
+	public String toString(){
+		
+		StringBuilder info = new StringBuilder();
+		info.append("Titulo: " + titulo
+				+ "\nArtista: " + artista);
+		if(faixas.size() > VAZIO){
+			if(this.getTrilhaPrincipal() == null){
+				info.append("\nTrilha Principal: Nao existe");
+			}else{
+				info.append("\nTrilha Principal: " + trilhaPrincipal);
+			}
+			info.append("\nFaixas: ");
+			for(int i = 0; i < faixas.size(); i++){
+				info.append("\n" + i+1 + " - " + this.getI(i));
+			}
+		}else{
+			info.append("\nTrilha Principal: Nao Existe"
+					+ "\nFaixas:\nVazio");
+			
+		}
+		
+		return info.toString();
+	}
+	
+	/**
+	 * Compara se dois CDs sao iguais
+	 * @param disco - Parametro que recebe um cd para ser comparado
+	 * @return - Retorna true ou false
+	 */
+	public boolean equals(CD disco){
+		if(disco.getArtista() == this.getArtista() && disco.getTitulo() == this.getTitulo()){
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ Lab06/src/lab06/TestaCD.java
@@ -0,0 +1,136 @@
+/* 
+ * ALUNO 1: Matheus Clemente Pereira
+ * ALUNO 2: Carlos Vinicius
+ */
+
+package lab06;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestaCD {
+	
+	private CD disco;
+	private String titulo = "Sao Joao";
+	private String artista = "Trio do Forro";
+	private final int NUM_FAIXAS = 3;
+	
+	@Before
+	public void criaObjetos() throws Exception {
+		disco = new CD(titulo, artista, NUM_FAIXAS);
+	}
+	
+	@Test
+	public void testaCriaCD(){
+		try {
+			new CD("titulo", "artista", -1);
+			Assert.fail("Esperava uma excecao, pois nao esta sendo passado parametros corretos");
+		}catch (Exception e){
+			Assert.assertEquals("Parametros passados estao incorretos",
+								e.getMessage());
+		}
+		
+		try {
+			new CD("titulo", "artista", 0);
+			Assert.fail("Esperava uma excecao, pois nao esta sendo passado parametros corretos");
+		}catch (Exception e){
+			Assert.assertEquals("Parametros passados estao incorretos",
+								e.getMessage());
+		}
+		
+		try{
+			CD cd = new CD("titulo", "artista");
+			Assert.assertTrue(cd.getM()==10);
+		}catch(Exception e){
+			//Nao faz nada;
+		}
+	}
+	
+	
+	@Test
+	public void testaMudaTrilhaPrincipal() throws Exception{
+		try {
+			CD cd = new CD("titulo", "artista", 1);
+			cd.setTrilhaPrincipal("musica");
+			Assert.fail("Esperava uma excecao, pois nao existem faixas no cd");
+		}catch (Exception e){
+			Assert.assertEquals("Faixa informada nao existe.", 
+								 e.getMessage());
+		}
+		
+		CD cd = new CD("titulo", "artista", 1);
+		cd.setFaixas("musica");
+		cd.setTrilhaPrincipal("musica");
+		Assert.assertTrue(cd.getTrilhaPrincipal() == "musica");
+	}
+	
+	@Test
+	public void testaMetodosAcessadores() throws Exception{
+		Assert.assertTrue(disco.getArtista() == artista);
+		Assert.assertFalse(disco.getArtista() == "atista errado");
+		
+		Assert.assertTrue(disco.getTitulo() == titulo);
+		Assert.assertFalse(disco.getTitulo() == "titulo errado");
+		
+		Assert.assertFalse(disco.getTrilhaPrincipal() == "musica");
+		disco.setFaixas("musica");
+		disco.setTrilhaPrincipal("musica");
+		Assert.assertTrue(disco.getTrilhaPrincipal() == "musica");
+		
+		Assert.assertTrue(disco.getM() == NUM_FAIXAS);
+		Assert.assertFalse(disco.getM() == 10);
+	}
+	
+	@Test
+	public void testaCadastroDeFaixas(){
+		Assert.assertTrue(disco.setFaixas("musica1"));
+		Assert.assertTrue(disco.setFaixas("musica2"));
+		Assert.assertTrue(disco.setFaixas("musica3"));
+		Assert.assertFalse(disco.setFaixas("musica4"));
+		
+		Assert.assertTrue(disco.getI(0) == "musica1");
+		Assert.assertTrue(disco.getI(3) == null);
+	}
+	
+	/*@Test
+	public void testaToString() throws Exception{
+		CD cd = new CD("Chimarruts Ao Vivo", "Chimarruts", 3);
+		
+		Assert.assertFalse(cd.toString() == "Titulo: Chimarruts Ao Vivo\n"
+				+ "Artista: Chimarruts\n"
+				+ "Trilha Principal: Não existe\n"
+				+ "Faixas:\nVazio");
+		
+		cd.setFaixas("Iemanja");
+		cd.setFaixas("Pra Ela");
+		cd.setFaixas("Floripa");
+		cd.setTrilhaPrincipal("Pra Ela");
+		Assert.assertTrue(cd.getTrilhaPrincipal() == "Pra Ela");
+		Assert.assertTrue(cd.toString() == "Titulo: Chimarruts Ao Vivo\n"
+				+ "Artista: Chimarruts\n"
+				+ "Trilha Principal: Pra Ela\n"
+				+ "Faixas:\n"
+				+ "1 - Iemanja\n"
+				+ "2 - Pra Ela\n"
+				+ "3 - Floripa");
+	}*/
+	
+	public void testaEquals() throws Exception{
+		CD disco2 = new CD(this.titulo, this.artista);
+		Assert.assertTrue(disco.equals(disco2));
+		
+		CD disco3 = new CD("aleatorio", "aleatorio");
+		Assert.assertFalse(disco.equals(disco3));
+	}
+	
+	public void main(String[] args) throws Exception{
+		CD cd = new CD("Chimarruts Ao Vivo", "Chimarruts", 3);
+		cd.setFaixas("Iemanja");
+		cd.setFaixas("Pra Ela");
+		cd.setFaixas("Floripa");
+		
+		cd.toString();
+	}
+	
+}
